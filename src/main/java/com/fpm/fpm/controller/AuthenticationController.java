1413af0a5bc37669ee99d1e030108ea1dd5c8328
bharath <127204988+bharathane@users.noreply.github.com>
2024-08-15 15:17:35 +0530

    add depoly url AuthenticationController.java

--- src/main/java/com/fpm/fpm/controller/AuthenticationController.java
+++ src/main/java/com/fpm/fpm/controller/AuthenticationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://fpm-frontend.vercel.app/")
 @RestController
 public class AuthenticationController {
 
